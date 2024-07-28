5df94d96d08a824d754b8421461ba758a7332620
Ahmed Sabri <asabri@e3r11p6.1337.ma>
2024-07-28 01:06:46 +0100

    I added twitter

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -5,6 +5,15 @@
 # ▐█▌▐█▪ ▄█▀▄ █▌▐█▌ ▐█.▪█▌▐█▌▐█▀▀█▄▐▀▀▪▄    ▐█· ▐█▌ ▄█▀▄ ██▪▐█▐▐▌▐█▐▐▌██▪   ▄█▀▄ ▄█▀▀█ ▐█· ▐█▌▐▀▀▪▄▐▀▀▄ 
 #  ▐█▀·.▐█▌.▐▌▐█▄█▌ ▐█▌·▐█▄█▌██▄▪▐█▐█▄▄▌    ██. ██ ▐█▌.▐▌▐█▌██▐█▌██▐█▌▐█▌▐▌▐█▌.▐▌▐█ ▪▐▌██. ██ ▐█▄▄▌▐█•█▌
 #   ▀ •  ▀█▄▀▪ ▀▀▀  ▀▀▀  ▀▀▀ ·▀▀▀▀  ▀▀▀     ▀▀▀▀▀•  ▀█▄▀▪ ▀▀▀▀ ▀▪▀▀ █▪.▀▀▀  ▀█▄▀▪ ▀  ▀ ▀▀▀▀▀•  ▀▀▀ .▀  ▀
+
+import yt_dlp
+import os
+import sys
+from contextlib import contextmanager
+
+Youtube = False
+Twitter = False
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,19 +25,64 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-import yt_dlp
 
+@contextmanager
+def suppress_output():
+    """A context manager to suppress stdout and stderr."""
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = devnull
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
+def YoutubeDownloader(url):
+    ydl_opts = {
+        'format': 'best',  # Specify the format
+        'outtmpl': '%(title)s.%(ext)s',  # Specify output template
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            # Start the download process
+            with suppress_output():
+                ydl.download([url])
+            print("Download complete")
+        except yt_dlp.utils.DownloadError as e:
+            # Handle any errors that occur during the download
+            print(f"An error occurred: {e}")
 
+def TwitterDownloader(url):
+    ydl_opts = {
+        'format': 'best',  # Specify the format
+        'outtmpl': '%(title)s.%(ext)s',  # Specify output template
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            # Start the download process
+            with suppress_output():
+                ydl.download([url])
+            print("Download complete")
+        except yt_dlp.utils.DownloadError as e:
+            # Handle any errors that occur during the download
+            print(f"An error occurred: {e}")
 
-ydl_opts = {}
-
-print("  ▄· ▄▌      ▄• ▄▌▄▄▄▄▄▄• ▄▌▄▄▄▄· ▄▄▄ .    ·▄▄▄▄        ▄▄▌ ▐ ▄▌ ▐ ▄ ▄▄▌         ▄▄▄· ·▄▄▄▄  ▄▄▄ .▄▄▄  ")
-print(" ▐█▪██▌▪     █▪██▌•██  █▪██▌▐█ ▀█▪▀▄.▀·    ██▪ ██ ▪     ██· █▌▐█•█▌▐███•  ▪     ▐█ ▀█ ██▪ ██ ▀▄.▀·▀▄ █·")
-print(" ▐█▌▐█▪ ▄█▀▄ █▌▐█▌ ▐█.▪█▌▐█▌▐█▀▀█▄▐▀▀▪▄    ▐█· ▐█▌ ▄█▀▄ ██▪▐█▐▐▌▐█▐▐▌██▪   ▄█▀▄ ▄█▀▀█ ▐█· ▐█▌▐▀▀▪▄▐▀▀▄ ")
-print("  ▐█▀·.▐█▌.▐▌▐█▄█▌ ▐█▌·▐█▄█▌██▄▪▐█▐█▄▄▌    ██. ██ ▐█▌.▐▌▐█▌██▐█▌██▐█▌▐█▌▐▌▐█▌.▐▌▐█ ▪▐▌██. ██ ▐█▄▄▌▐█•█▌")
-print("   ▀ •  ▀█▄▀▪ ▀▀▀  ▀▀▀  ▀▀▀ ·▀▀▀▀  ▀▀▀     ▀▀▀▀▀•  ▀█▄▀▪ ▀▀▀▀ ▀▪▀▀ █▪.▀▀▀  ▀█▄▀▪ ▀  ▀ ▀▀▀▀▀•  ▀▀▀ .▀  ▀")
-print("Enter the link of the video you want to download")
-url = input()
-
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+if __name__ == "__main__":
+    print(f"{bcolors.OKBLUE}  ▄· ▄▌      ▄• ▄▌▄▄▄▄▄▄• ▄▌▄▄▄▄· ▄▄▄ .    ·▄▄▄▄        ▄▄▌ ▐ ▄▌ ▐ ▄ ▄▄▌         ▄▄▄· ·▄▄▄▄  ▄▄▄ .▄▄▄  ")    
+    print(f"{bcolors.HEADER} ▐█▪██▌▪     █▪██▌•██  █▪██▌▐█ ▀█▪▀▄.▀·    ██▪ ██ ▪     ██· █▌▐█•█▌▐███•  ▪     ▐█ ▀█ ██▪ ██ ▀▄.▀·▀▄ █·")    
+    print(f"{bcolors.HEADER} ▐█▌▐█▪ ▄█▀▄ █▌▐█▌ ▐█.▪█▌▐█▌▐█▀▀█▄▐▀▀▪▄    ▐█· ▐█▌ ▄█▀▄ ██▪▐█▐▐▌▐█▐▐▌██▪   ▄█▀▄ ▄█▀▀█ ▐█· ▐█▌▐▀▀▪▄▐▀▀▄ ")    
+    print(f"{bcolors.HEADER}  ▐█▀·.▐█▌.▐▌▐█▄█▌ ▐█▌·▐█▄█▌██▄▪▐█▐█▄▄▌    ██. ██ ▐█▌.▐▌▐█▌██▐█▌██▐█▌▐█▌▐▌▐█▌.▐▌▐█ ▪▐▌██. ██ ▐█▄▄▌▐█•█▌")    
+    print(f"{bcolors.OKBLUE}   ▀ •  ▀█▄▀▪ ▀▀▀  ▀▀▀  ▀▀▀ ·▀▀▀▀  ▀▀▀     ▀▀▀▀▀•  ▀█▄▀▪ ▀▀▀▀ ▀▪▀▀ █▪.▀▀▀  ▀█▄▀▪ ▀  ▀ ▀▀▀▀▀•  ▀▀▀ .▀  ▀")    
+    print("1: Youtube")
+    print("2: Twitter")
+    print("Enter the number ")
+    number = input()
+    print(f"{bcolors.HEADER}Enter the link of the video you want to download")
+    url = input()
+    if (number == "1"):
+        YoutubeDownloader(url)
+    else :
+        TwitterDownloader(url)
+    
